de8269a655d92026bc00908b513f696b51406793
Sundar P <sudarmahii716@gmail.c0m>
2026-07-09 14:20:41 +0530

    forms

--- forms.js
+++ forms.js
@@ -4,7 +4,28 @@ function getFormValues(event) {
     const name = document.getElementById('name').value;
     const mark = document.getElementById('mp').value;
 
-    document.getElementById('studentName').innerHTML = name;
-    document.getElementById('studentMP').innerHTML = mark;
+    let error = false;
+
+    if (name.trim() === '') {
+        const nameErr = document.getElementById('nameError');
+
+        nameErr.innerHTML = 'Name Cannot be Empty';
+
+        nameErr.style.color = 'red';
+        error = true;
+    }
+
+    if (!mark || (mark < 0 || mark > 100)) {
+        document.getElementById('mpError').innerHTML = 'Invalid Mark. Mark should be within 0 to 100';
+
+        error = true;
+    }
+
+    if (!error && name.trim() && mark >= 0 && mark <= 100) {
+        document.getElementById('studentName').innerHTML = name;
+
+        document.getElementById('studentMP').innerHTML = mark;
+
+    }
 
 }
